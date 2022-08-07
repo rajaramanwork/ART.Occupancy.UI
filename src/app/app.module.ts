285997a8caa55b7d8d28b3995a8e4d9b57af2dbf
rajaramanwork <rajaraman.work@gmail.com>
2022-08-07 01:38:45 +0000

    Setup Separate Views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,16 +12,23 @@ import { AppComponent } from './app.component';
 /* PrimeNG Imports*/
 import {InputSwitchModule} from 'primeng/inputswitch';
 
+/* Custom App Components */
+import { ProductListComponent } from '../app/features/product-list/product-list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    InputSwitchModule
+    InputSwitchModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
