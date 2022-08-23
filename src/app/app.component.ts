d0e0850e31d95cc19bb71fa32f746119f7f49655
rajaramanwork <rajaraman.work@gmail.com>
2022-08-23 04:55:11 +0000

    Integrated Dashboard Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation,ViewChild  } from '@angular/core';
 import { SidebarComponent, ClickEventArgs } from '@syncfusion/ej2-angular-navigations';
+import { DashboardLayoutComponent, PanelModel } from '@syncfusion/ej2-angular-layouts';
+
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,29 @@ export class AppComponent {
   public enableDock: boolean = true;
   public width: string = '220px';
   public dockSize: string = '72px';
+  public cellSpacing: number[] = [10, 10];
   toggleClick() {
       this.dockBar.toggle();
   }
+
+  public dashboardObject: DashboardLayoutComponent;
+  public count: number = 5;
+  public panelsData: PanelModel[] = [{
+      'sizeX': 2, 'sizeY': 2, 'row': 0, 'col': 0, header: '<div>Panel 1</div>'
+  },
+  {
+      'sizeX': 2, 'sizeY': 2, 'row': 0, 'col': 2, header: '<div>Panel 2</div>'
+  },
+  {
+      'sizeX': 2, 'sizeY': 2, 'row': 0, 'col': 4, header: '<div>Panel 3</div>'
+  },
+  {
+      'sizeX': 4, 'sizeY': 2, 'row': 2, 'col': 0, header: '<div>Panel 4</div>'
+  },
+  {
+      'sizeX': 2, 'sizeY': 2, 'row': 2, 'col': 4, header: '<div>Panel 5</div>'
+  }];
+  public headerCount: number = 1;
   
   constructor() {
       
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { CircularGaugeModule, GaugeTooltipService, LegendService } from '@syncfu
 import { VirtualizationService,ListViewAllModule } from '@syncfusion/ej2-angular-lists';
 import { ToolbarModule, SidebarModule } from '@syncfusion/ej2-angular-navigations';
 import {  ChartAllModule, AccumulationChartAllModule, RangeNavigatorAllModule } from '@syncfusion/ej2-angular-charts';
+import { DashboardLayoutModule } from '@syncfusion/ej2-angular-layouts';
 
 //registerLicense('ORg4AjUWIQA/Gnt2VVhiQlFaclxJVHxIeEx0RWFbb196d1NMY1VBNQtUQF1hS35bd0JjWHpXcnNdT2Za');
 registerLicense('Mgo+DSMBaFt/QHJqVVhjWlpFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQF9jQX9bdEVjWXxddXdWRg==;Mgo+DSMBPh8sVXJ0S0R+XE9HcFRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS3xTfkRrW39ecHdWQ2RbUQ==;ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdk1iUX1ecHRWRWVbUEQ=;NzAwMzMwQDMyMzAyZTMyMmUzMFRjbU1LeFJadkhwQS9jeTJOSzl2cEljdW9xQnVFakdvcFYyOENOVmZZTWM9;NzAwMzMxQDMyMzAyZTMyMmUzME9UNnIvbnAzQ01DRzRCNENXNW1VYUdBR2RhL09HNm9tV3JPNXZxT0g0VWs9;NRAiBiAaIQQuGjN/V0Z+Xk9EaFxEVmJLYVB3WmpQdldgdVRMZVVbQX9PIiBoS35RdEVrWHdccHRVRWNbU0B2;NzAwMzMzQDMyMzAyZTMyMmUzMFc0eFc2Qm5KeitjRzNVdHBXUWd1d2N4b0s2WkpxQ28rR3ZBbWV6UDNXMlk9;NzAwMzM0QDMyMzAyZTMyMmUzMFh0R1FEUCtmVXkxWXF3TmkvbEMzb2NSeW5PODAwc3g0aDBWR3dacjNzbTg9;Mgo+DSMBMAY9C3t2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdk1iUX1ecHRWRWhZWU0=;NzAwMzM2QDMyMzAyZTMyMmUzMEc0dGNBYlNpaEJ2cjRSM3psR1BXek1mU2hkamZIR1grWXo0OVkva1FJTEU9;NzAwMzM3QDMyMzAyZTMyMmUzMGY4cGR1b2U4S1BLTWRhanlwYUFiWWlmYnl1NjI5N1UrdzNZLzhxRHVYMG89;NzAwMzM4QDMyMzAyZTMyMmUzMFc0eFc2Qm5KeitjRzNVdHBXUWd1d2N4b0s2WkpxQ28rR3ZBbWV6UDNXMlk9')
@@ -47,7 +48,8 @@ import { OccupancyRatesComponent } from './features/occupancy-rates/occupancy-ra
     RangeNavigatorAllModule,
     SidebarModule,
     ListViewAllModule,
-    ToolbarModule
+    ToolbarModule,
+    DashboardLayoutModule 
   ],
   providers: [GaugeTooltipService, LegendService],
   bootstrap: [AppComponent]
