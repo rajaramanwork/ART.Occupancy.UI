4e9593b63689e75427a0e1d5dd2268cb0227517f
rajaramanwork <rajaraman.work@gmail.com>
2022-08-24 20:16:50 +0000

    Circular gauge customization

--- src/app/features/occupancy-rates/occupancy-rates.component.ts
+++ src/app/features/occupancy-rates/occupancy-rates.component.ts
@@ -8,104 +8,12 @@ import { Font } from '@syncfusion/ej2-angular-charts';
   encapsulation: ViewEncapsulation.None
 })
 export class OccupancyRatesComponent implements OnInit {
-  
-  /*public legendSettings: Object = {
-    visible: true,
-    toggleVisibility: true,
-    position: 'Right'
-  };;
-
-  public tooltip: Object =  {
-    type:['Pointer', 'Range' ],
-    enable: true,
-    enableAnimation: false,
-    rangeSettings: { fill:'red' }
-  };
-  
-  public margin: Object = {
-    left: 0, right: 0, top: 0, bottom: 0
-  };
-
-  public axisIndex: number = 0;
-  public lineStyle1: Object = {
-      width: 1.5
-  };
-
-  // custom code end
-  //Initializing MajorTicks
-  public majorTicks1: Object = {
-      position: 'Inside',
-      width: 2,
-      height: 10,
-      color: '#843c54'
-  };
-  public title: string = 'Occupancy Rates';
-  //Initializing TitleStyle
-  public titleStyle: Object = {
-      color: 'gray',
-      size: '16px',
-      font: { fontweight:'bold' }
-  };
-  public minorTicks1: Object = {
-      position: 'Inside',
-      width: 2,
-      height: 5,
-      color: '#843c54'
-  };
-  public markerHeight: number = 15;
-  public markerWidth: number = 15;
-  public labelStyle1: Object = {
-      position: 'Inside',
-      autoAngle: true,
-      hiddenLabel: 'None'
-  };
-  public cap: Object = { color: 'white', radius: 0, border: { width: 0 } };
-
-  public lineStyle2: Object = { width: 1.5, color: '#E84011' };
-
-  public labelStyle2: Object = {
-      position: 'Outside',
-      autoAngle: true,
-      hiddenLabel: 'None',
-      font: { color: '#E84011' }
-  };
-  public majorTicks2: Object = {
-      position: 'Outside',
-      width: 2,
-      height: 10,
-      color: '#E84011'
-  };
-  public minorTicks2: Object = {
-      position: 'Outside',
-      width: 2,
-      height: 5,
-      color: '#E84011'
-  };
-
-  public lineStyle3: Object = { width: 1.5, color: '#66cdaa' };
-
-  public labelStyle3: Object = {
-      position: 'Outside',
-      autoAngle: true,
-      hiddenLabel: 'None',
-      font: { color: '#E84011' }
-  };
-  public majorTicks3: Object = {
-      position: 'Outside',
-      width: 2,
-      height: 10,
-      color: '#66cdaa'
-  };
-  public minorTicks3: Object = {
-      position: 'Outside',
-      width: 2,
-      height: 5,
-      color: '#66cdaa'
-  };*/
   public lineStyle: Object;
   public majorTicks: Object;
   public minorTicks: Object;
   public legendSettings: object;
+  public tooltip: Object;
+  public annotations:any;
   ngOnInit(): void {
    // Initialize objects.
    this.lineStyle = {
@@ -121,5 +29,16 @@ export class OccupancyRatesComponent implements OnInit {
         visible: true,
         toggleVisibility: true
     };
+    this.tooltip = {
+        type:['Pointer', 'Range', 'Annotation'],
+        enable: true,
+        enableAnimation: false,
+        annotationSettings: { template:'<div>CircularGauge</div>' },
+        rangeSettings: { fill:'red' }
+    };
+    this.annotations = [{
+        content: 'CircularGauge',
+        angle: 180, zIndex: '1',
+    }];
   }
 }
